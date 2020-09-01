f858f5544c569c89a1774ec28e38746b8ef4ac1b
PreslavaKuzova <preslava.kuzova@gmail.com>
2020-09-01 20:49:53 +0300

    adding base UI design and fargments

---
+++ app/src/main/java/com/example/chitchat/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.chitchat;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.chitchat.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private ChatsFragment chatsFragment;
+    private GroupChatsFragment groupChatsFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initLocalVariables();
+        setUpToolbar();
+
+        setUpBottomNavigationProperties();
+
+    }
+
+    private void setUpBottomNavigationProperties() {
+        final FragmentManager manager = getSupportFragmentManager();
+
+        binding.bottomNavBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                FragmentTransaction transaction = manager.beginTransaction();
+
+                if (item.getItemId() == R.id.chats_page) {
+                    transaction.replace(R.id.grp_container, chatsFragment);
+                    transaction.commit();
+                } else if (item.getItemId() == R.id.group_chats_page) {
+                    transaction.replace(R.id.grp_container, groupChatsFragment);
+                    transaction.commit();
+                }
+                return true;
+            }
+        });
+
+        binding.bottomNavBar.setSelectedItemId(R.id.chats_page);
+    }
+
+    private void initLocalVariables() {
+        this.binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        this.chatsFragment = new ChatsFragment();
+        this.groupChatsFragment = new GroupChatsFragment();
+    }
+
+    private void setUpToolbar() {
+        setSupportActionBar(binding.toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+    }
+
+}
